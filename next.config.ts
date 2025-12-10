9ac568ce433431df1c579e8ac59591bf8e46b788
HHH-wyx <2512492141@qq.com>
2025-12-10 23:47:16 +0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // i18n configuration is handled in App Router with generateStaticParams
+};
+
+export default nextConfig;
